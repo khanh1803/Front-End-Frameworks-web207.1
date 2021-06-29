00b4eae4c5de75d741774c323b7fe33abbc9098f
khanh ph10608 <khanhndph10608@fpt.edu.vn>
2021-06-29 16:36:45 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const person = {
+  name:'nguyen duc khanh',
+  age:20,
+  status:false
+}
+function total(giatriA, giatriB){
+  return giatriA + giatriB;
+}
+const a = 20;
+const b =20;
+const element = <h1>tổng là  : {total(a,b)}</h1>
+
+ReactDOM.render(
+element,
+
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
